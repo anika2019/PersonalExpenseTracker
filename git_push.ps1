2e2e050e4e71f41ef608d4bc864dec1c6e733be2
ajit yadav <ajit.2603@gmail.com>
2026-03-13 08:47:32 +0530

    Remove temporary git_push.ps1 script

--- git_push.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "d:\Desktop\OneDrive - shivameducation.net\Documents\AJIT_YADAV\CodeBasicsBootCamp\CodeBasicsProjects\Python-Projects\PersonalExpenseTracker"
-git status
-git add -A
-git commit -m "Move Dockerfile to root directory"
-git push
